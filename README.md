fff0a967850c6062d225c152140d958e807b705f
Isaac Wu <isaacwu2000@gmail.com>
2024-12-20 18:44:58 +0000

    Changes

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Added serviceAccountKey.json in a .gitignore
 install node
 npm install firebase
+npm install firebase@11.1.0 --save
--- firestoreserver.py
+++ firestoreserver.py
@@ -4,3 +4,12 @@ from firebase_admin import credentials, auth
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# Reading the collection
+collection_ref = db.collection('users')
+docs = collection_ref.stream()
+
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
